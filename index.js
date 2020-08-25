c31cd500153f40144658b569cf5210643161db09
Nathan Birus <nathan.birus@nih.gov>
2020-08-25 14:35:50 -0500

    inits

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var port = process.env.PORT || 3000
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+http.listen(port, function () {
+  console.log('listening on *:' + port)
+})
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+// If the URL of the socket server is opened in a browser
+function handler(request, response) {
+  fileServer.serve(request, response)
+  // request.addListener('end', function () {
+  //   fileServer.serve(request, response) // this will return the correct file
+  // })
+}
+
+// Delete this row if you want to see debug messages
+io.set('log level', 1)
+
+// Listen for incoming connections from clients
+io.sockets.on('connection', function (socket) {
+  // Start listening for mouse move events
+  socket.on('mousemove', function (data) {
+    // This line sends the event (broadcasts it)
+    // to everyone except the originating client.
+    socket.broadcast.emit('moving', data)
+  })
+})
