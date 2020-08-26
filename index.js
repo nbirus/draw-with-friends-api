f2a0918e0ca0e66400f1acea13b57fe5df2a3ce4
Nathan Birus <nathan.birus@nih.gov>
2020-08-25 20:12:42 -0500

    updates

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const e = require('express')
+
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var port = process.env.PORT || 3000
+
+let lobby = []
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
@@ -11,21 +15,26 @@ http.listen(port, function () {
   console.log('listening on *:' + port)
 })
 
-// If the URL of the socket server is opened in a browser
-function handler(request, response) {
-  fileServer.serve(request, response)
-  // request.addListener('end', function () {
-  //   fileServer.serve(request, response) // this will return the correct file
-  // })
-}
-
 // Delete this row if you want to see debug messages
 io.set('log level', 1)
 
 // Listen for incoming connections from clients
 io.sockets.on('connection', function (socket) {
+  // Start listening for players joining
+  socket.on('join', function (data) {
+    let playerIndex = lobby.findIndex((player) => player.id === data.id)
+    if (playerIndex === -1) {
+      lobby.push(data)
+    } else {
+      lobby[playerIndex].username = data.username
+    }
+
+    socket.emit('lobby', lobby)
+  })
+
   // Start listening for mouse move events
   socket.on('mousemove', function (data) {
+    console.log(data)
     // This line sends the event (broadcasts it)
     // to everyone except the originating client.
     socket.broadcast.emit('moving', data)
